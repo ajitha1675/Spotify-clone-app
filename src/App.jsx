1f942260e5e1fe86e6128c4d5bda70d1c9515d98
Ajilalitha <ajilalitha010@gmail.com>
2024-11-13 14:53:51 +0530

    Add the files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SideBar from './Components/SideBar'
+
+const App = () => {
+  return (
+    <div className='h-screen bg-black'>
+      <div className='h-[90%] flex'>
+       <SideBar />
+      </div>
+    </div>
+  )
+}
+
+export default App
